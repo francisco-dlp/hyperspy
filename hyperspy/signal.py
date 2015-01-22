2d8170cf082b50e82505639bdf14d15781ca86bf
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-01-22 01:47:09 +0100

    _apply_funtion_on_... handles shape change
    If the shape of 'out' signal changes as a consequence of
    Signal._apply_function_on_data_and_remove_axis, then it now calls
    out.get_dimensions_from_data() to update the axes_manager.
    get_dimensions_from_data is also decorated with @auto_replot, so if
    previously plotted, it will trigger a replot.
    
    Conflicts:
    	hyperspy/signal.py

--- hyperspy/signal.py
+++ hyperspy/signal.py
@@ -3597,7 +3597,10 @@ class Signal(MVA,
             self.metadata.Signal.record_by = self._record_by
             self._assign_subclass()
 
-    def _apply_function_on_data_and_remove_axis(self, function, axis):
+    def _apply_function_on_data_and_remove_axis(self, function, axis,
+                                                out=None):
+        if out is not None:
+            oldshape = out.data.shape
         if axis not in ("navigation", "signal"):
             s = self._deepcopy_with_new_data(
                 function(self.data,
@@ -3620,7 +3623,12 @@ class Signal(MVA,
             data = function(data,
                             axis=iaxes.pop())
         s.data[:] = data
-        return s
+        if out is None:
+            return s
+        else:
+            if oldshape != out.data.shape:
+                out.get_dimensions_from_data()
+            out.events.data_changed.trigger()
 
     def sum(self, axis="navigation"):
         """Sum the data over the given axis.
