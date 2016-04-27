d5332808b756f57e32c7692cd76bac761e3cb9f7
Petras <klavishas@gmail.com>
2016-04-27 13:44:47 +0200

    revert accidently changed master

--- hyperspy/io_plugins/__init__.py
+++ hyperspy/io_plugins/__init__.py
@@ -20,9 +20,9 @@
 import logging
 
 from hyperspy.io_plugins import (msa, digital_micrograph, fei, mrc, ripple,
-                                 tiff, semper_unf, blockfile, dens, bcf)
+                                 tiff, semper_unf, blockfile, dens)
 io_plugins = [msa, digital_micrograph, fei, mrc, ripple, tiff, semper_unf,
-              blockfile, dens, bcf]
+              blockfile, dens]
 
 _logger = logging.getLogger(__name__)
 
