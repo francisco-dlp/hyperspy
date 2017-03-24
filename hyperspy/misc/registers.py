0b2504a13b75f2a9625942dbd063e33634e862c1
Francisco de la Peña <frandelapena@gmail.com>
2017-03-24 16:36:21 +0000

    Automatic style corrections courtesy of autopep8

--- hyperspy/misc/registers.py
+++ hyperspy/misc/registers.py
@@ -1,6 +1,7 @@
 
 from hyperspy import signals, components1d, components2d
 
+
 def register_component1d(component):
     """Decorator to register a  1D component.
 
@@ -9,12 +10,13 @@ def register_component1d(component):
     NameError: If a component of the same name is already in the registry.
 
     """
-    name =  component.__name__
+    name = component.__name__
     if hasattr(components1d, name):
         raise NameError(
             "A component of the same name has already been registered")
     setattr(components1d, name, component)
     return component
+
 
 def register_component2d(component):
     """Decorator to register a  1D component.
@@ -24,12 +26,13 @@ def register_component2d(component):
     NameError: If a component of the same name is already in the registry.
 
     """
-    name =  component.__name__
+    name = component.__name__
     if hasattr(components2d, name):
         raise NameError(
             "A component of the same name has already been registered")
     setattr(components1d, name, component)
     return component
+
 
 def register_signal(component):
     """Decorator to register a Signal class.
@@ -39,7 +42,7 @@ def register_signal(component):
     NameError: If a signal of the same name is already in the registry.
 
     """
-    name =  signal.__name__
+    name = signal.__name__
     if hasattr(signals, name):
         raise NameError(
             "A Signal of the same name has already been registered")
